568b473faef227b115205ad6e4b72821a9edcf5a
ashok-cdw <125631918+ashok-cdw@users.noreply.github.com>
2023-04-10 12:20:04 +0530

    Update HelloController.java

--- Task - 2/src/main/java/com/example/demo/HelloController.java
+++ Task - 2/src/main/java/com/example/demo/HelloController.java
@@ -4,35 +4,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.MatrixVariable;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping(path = "hello")
 public class HelloController {
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping
 	public void sayHai() {
 		System.out.println("Hai...We are from CDW");
 	}
 
-	@RequestMapping(method = RequestMethod.GET, value = "hello2")
+	@GetMapping("hello2")
 	public String sayHai2() {
 		System.out.println("Hellooo222.....");
 		return "welcome";
 	}
 
-	@RequestMapping(method = RequestMethod.GET, value = "hello3")
+	@GetMapping("hello3")
 	public ResponseEntity<String> sayHai3() {
 		return ResponseEntity.ok("This is a response from ResponseEntity");
 	}
 
-	@RequestMapping(method = RequestMethod.GET, value = "hello4")
+	@GetMapping("hello4")
 	public ModelAndView sayHai4() {
 		ModelAndView mv = new ModelAndView();
 		mv.addObject("mykey", "Welcome to CDW, Value from ModelAndView");
@@ -40,14 +34,14 @@ public class HelloController {
 		return mv;
 	}
 
-	@RequestMapping(method = RequestMethod.GET, value = "hello5")
+	@GetMapping("hello5")
 	public ModelAndView sayHai5(ModelAndView mv) {
 		mv.addObject("mykey", "Welcome to CDW, Value from ModelAndView....Injected by View Resolver");
 		mv.setViewName("welcome");
 		return mv;
 	}
 
-	@RequestMapping(method = RequestMethod.GET, value = "hello6")
+	@GetMapping("hello6")
 	public ModelAndView sayHai6(ModelAndView mv, HttpServletRequest request) {
 		mv.addObject("mykey", "Welcome to CDW, Value from ModelAndView....Injected by View Resolver");
 		mv.setViewName("welcome");
@@ -58,7 +52,7 @@ public class HelloController {
 		return mv;
 	}
 
-	@RequestMapping(method = RequestMethod.GET, value = "repeat")
+	@GetMapping("repeat")
 	public ModelAndView repeat(ModelAndView mv, HttpServletRequest request) {
 		mv.addObject("mykey", "Welcome to CDW, Value from ModelAndView....repeat repeat repeat");
 		mv.setViewName("welcome");
@@ -66,7 +60,7 @@ public class HelloController {
 		return mv;
 	}
 
-	@RequestMapping(method = RequestMethod.GET, value = "hello7/{name}")
+	@GetMapping("hello7/{name}")
 	public ModelAndView hello7(@PathVariable String name, ModelAndView mv, HttpServletRequest request) {
 		mv.addObject("mykey", "Welcome to CDW, Value from ModelAndView....");
 		mv.setViewName("welcome");
@@ -74,7 +68,7 @@ public class HelloController {
 		return mv;
 	}
 
-	@RequestMapping(method = RequestMethod.GET, value = "hello8")
+	@GetMapping("hello8")
 	public ModelAndView hello8(@RequestParam("uname") String name, ModelAndView mv, HttpServletRequest request) {
 		mv.addObject("mykey", "Welcome to CDW, Value from ModelAndView....");
 		mv.setViewName("welcome");
@@ -82,56 +76,61 @@ public class HelloController {
 		return mv;
 	}
 
-	@RequestMapping(method = RequestMethod.GET, value = "matrix")
-	public ModelAndView matrix(@MatrixVariable(name = "uname", defaultValue = "ashok") String name, ModelAndView mv, HttpServletRequest request) {
+	@GetMapping("matrix")
+	public ModelAndView matrix(@MatrixVariable(name = "uname", defaultValue = "ashok") String name, ModelAndView mv,
+			HttpServletRequest request) {
 		mv.addObject("mykey", "Welcome to CDW, Value from ModelAndView....");
 		mv.setViewName("welcome");
 		request.setAttribute("hello1", name + " comes from matrix variable");
 		return mv;
 	}
-	
-	@RequestMapping(method = RequestMethod.POST, value = "hello9")
-	public ResponseEntity<User> hello9(){
+
+	@PostMapping("hello9")
+	public ResponseEntity<User> hello9() {
 		User user = new User();
-		user.setUid(2022);
-		user.setUname("Ashok");
+		user.setUserId(2022);
+		user.setUserName("Ashok");
 		return ResponseEntity.ok(user);
 	}
-	
-	@RequestMapping(method = RequestMethod.POST, value = "hello10")
-	public User hello10(){
+
+	@PostMapping("hello10")
+	public User hello10() {
 		User user = new User();
-		user.setUid(1234);
-		user.setUname("Ramu");
+		user.setUserId(1234);
+		user.setUserName("Ramu");
 		return user;
 	}
-	
-	@RequestMapping(method = RequestMethod.POST, value = "hello11")
-	public User hello11(@RequestBody User user){
-		user.setUid(1010);
-		user.setUname("---vjvh---");
+
+	@PostMapping("hello11")
+	public User hello11(@RequestBody User user) {
+		user.setUserId(1010);
+		user.setUserName("---vjvh---");
 		return user;
 	}
-	
-	@RequestMapping(method = RequestMethod.POST, value = "sayhai")
-	public String hello12(@RequestBody User user){
-		return "Haii... " + user.getUname()+"\n";
+
+	@PostMapping("sayhai")
+	public String hello12(@RequestBody User user) {
+		return "Haii... " + user.getUserName() + "\n";
 	}
 }
 
-class User{
-	int uid;
-	String uname;
-	public int getUid() {
-		return uid;
+class User {
+	private int userId;
+	private String userName;
+
+	public int getUserId() {
+		return userId;
 	}
-	public void setUid(int uid) {
-		this.uid = uid;
+
+	public void setUserId(int userId) {
+		this.userId = userId;
 	}
-	public String getUname() {
-		return uname;
+
+	public String getUserName() {
+		return userName;
 	}
-	public void setUname(String uname) {
-		this.uname = uname;
+
+	public void setUserName(String userName) {
+		this.userName = userName;
 	}
 }
